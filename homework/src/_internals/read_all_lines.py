34925d95467eba64066a07f03e8af1abc1e972ed
juanfelipecalleb <jcalleb@unal.edu.co>
2025-12-06 09:52:10 -0500

    se carga completo

--- homework/src/_internals/read_all_lines.py
+++ homework/src/_internals/read_all_lines.py
@@ -2,8 +2,10 @@ import os
 
 
 def read_all_lines(input_folder):
-    all_lines = []
+
+    lines = []
     for filename in os.listdir(input_folder):
-        with open(os.path.join(input_folder, filename), "r", encoding="utf-8") as f:
-            all_lines.extend(f.readlines())
-    return all_lines
+        file_path = os.path.join(input_folder, filename)
+        with open(file_path, "r", encoding="utf-8") as f:
+            lines.extend(f.readlines())
+    return lines
